0d67cb98fdb5624a3bedfe9769e683e25fe2a7e1
sou91 <sourabh.sontakke91@gmail.com>
2021-03-27 01:22:07 +0530

    svr change

--- server/index.js
+++ server/index.js
@@ -84,7 +84,7 @@ if (!isDev && cluster.isMaster) {
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function (request, response) {
-   //response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
   });
 
   app.listen(PORT, function () {
